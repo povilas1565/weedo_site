708bbe4209332a066990191e879a78e027837733
Paul156551 <pavel.ryzhkov.99@mail.ru>
2025-07-30 23:11:35 +0300

    Create superuser

---
+++ products/migrations/0002_create_superuser.py
@@ -0,0 +1,24 @@
+﻿from django.db import migrations
+from django.contrib.auth.hashers import make_password
+
+def create_superuser(apps, schema_editor):
+    User = apps.get_model('auth', 'User')
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create(
+            username='admin',
+            email='test@example.com',
+            password=make_password('test123'),
+            is_staff=True,
+            is_superuser=True,
+            is_active=True,
+        )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('products', '0001_initial'), 
+    ]
+
+    operations = [
+        migrations.RunPython(create_superuser),
+    ]
